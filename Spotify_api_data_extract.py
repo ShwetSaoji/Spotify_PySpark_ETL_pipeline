f929d4d0d34039a85b27ee70c956f053ecfc2af0
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-05-01 22:17:00 -0700

    Update Spotify_api_data_extract.py

--- Spotify_api_data_extract.py
+++ Spotify_api_data_extract.py
@@ -6,25 +6,24 @@ import boto3
 from datetime import datetime
 
 def lambda_handler(event, context):
-    
-    cilent_id = os.environ.get('client_id')
+    client_id = os.environ.get('client_id')
     client_secret = os.environ.get('client_secret')
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    sp=spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     
-    client_credentials_manager = SpotifyClientCredentials(client_id=cilent_id, client_secret=client_secret)
-    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-    playlists = sp.user_playlists('spotify')
+    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbLRQDuF5jeBp"
+    playlist_URI = playlist_link.split("/")[-1]
+    spotify_data = sp.playlist_tracks(playlist_URI)
     
-    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
-    playlist_URI = playlist_link.split("/")[-1].split("?")[0]
-    
-    spotify_data = sp.playlist_tracks(playlist_URI)   
+    print(spotify_data)
     
     cilent = boto3.client('s3')
     
     filename = "spotify_raw_" + str(datetime.now()) + ".json"
     
     cilent.put_object(
-        Bucket="spotify-etl-project-darshil",
+        Bucket="spotify-etl-project-shwet",
         Key="raw_data/to_processed/" + filename,
         Body=json.dumps(spotify_data)
         )
+    
